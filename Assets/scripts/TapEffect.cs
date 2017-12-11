460b613108ee3849f539eb4371d698302518841a
n2ij <info@n2gdl.net>
2017-12-11 14:17:13 +0900

    自機を動かすテスト。画面タップテスト

---
+++ Assets/scripts/TapEffect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TapEffect : MonoBehaviour
+{
+	[SerializeField] ParticleSystem tapEffect;              // タップエフェクト
+//	[SerializeField] Camera _camera;                        // カメラの座標
+
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			// マウスのワールド座標までパーティクルを移動し、パーティクルエフェクトを1つ生成する
+			Vector3 mousePos = GodTouches.GodTouch.GetPosition ();
+			mousePos.z = 5.0f;
+			Vector3 tmpPos2 = Camera.main.ScreenToWorldPoint (mousePos);
+
+//			var pos = _camera.ScreenToWorldPoint(Input.mousePosition + _camera.transform.forward * 100);
+			tapEffect.transform.position = tmpPos2;
+			tapEffect.Emit(1);
+			Debug.Log("tmpPos == " + tmpPos2 , this);
+		}
+	}
+}
---
+++ Assets/scripts/shipControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shipControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// 移動スピード
+	public float speed = 3;
+	private GameObject _child;
+	
+	// Update is called once per frame
+	void Update () {
+		// 右・左
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		// 上・下
+		float z = Input.GetAxisRaw ("Vertical");
+
+		// 移動する向きを求める
+		Vector3 direction = new Vector3 (x, 0.0f, z).normalized;
+
+		// 移動する向きとスピードを代入する
+		_child = transform.FindChild ("spaceShip").gameObject;
+		_child.GetComponent<Rigidbody>().velocity = direction * speed;
+	}
+}
