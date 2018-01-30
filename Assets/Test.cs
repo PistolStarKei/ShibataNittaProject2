8d910390528de7ce0b73c8e3e4c24411f0be9868
柴田圭 <info@pistolstarweb.com>
2018-01-31 03:18:23 +0900

    不要なシーンの削除（PrefabTest TestForSS）: フライカメラの値変更

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,9 +1,30 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class Test :MonoBehaviour {
 	
+	Dictionary<string, int[]> ArrayLst = new Dictionary<string, int[]>();
+	int[] a = new int[] {1,2,3,4};
+	int[] b = new int[] {4,3,2,1,0};
+
+
+
+	void Start () {
+		ArrayLst.Add("a", a);
+		ArrayLst.Add("b", b);
+
+		string ArrayName = "a";
+		int[] c = null;
+		if (ArrayLst.ContainsKey(ArrayName))
+		{
+			c = ArrayLst[ArrayName];
+		}
+
+		Debug.Log("Variable value: " + c.Length);
+	
+	}
 
 }
