68fec1c13c4a0522668634cc0bb08bb5b1b0dd63
n2ij <info@n2gdl.net>
2018-02-03 05:02:41 +0900

    ステージをゲームに追加する処理

--- Assets/Scripts/mapControl.cs
+++ Assets/Scripts/mapControl.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class mapControl : MonoBehaviour {
 
 	public int mapNumber = 0;
+	public GameObject stage00;
+	public GameObject stage01;
+	public GameObject stage02;
+	public GameObject stage03;
 
 	public void setMapNumber(int num){
 		mapNumber = num;
@@ -11,61 +15,26 @@ public class mapControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-//		attachMapData ();
-//		setMapBlockTag ();
-//		changeLayerNameToWall ();
+		attachMapData ();
 	}
 
 	public void attachMapData(){
-//		mapNumber;
-
 		// mapManagerの子供にmapDataをつける処理
 		GameObject mapManager = GameObject.Find ("mapManager");
-//		string str = sprintf ("stage%02d", mapNumber);
 
-	}
-
-	// MapChunk以下のmapオブジェクトすべてにMapBlockタグをつける
-	public void setMapBlockTag (){
-
-		GameObject mapManager = GameObject.Find ("mapManager");
-
-		Transform[] transformArray = mapManager.GetComponentsInChildren<Transform>();
-		foreach (Transform child in transformArray)
-		{
-			child.tag = "MapBlock";
+		switch (mapNumber) {
+		case 0:
+			Instantiate (stage00);
+			break;
+		case 1:
+			Instantiate (stage01);
+			break;
+		case 2:
+			Instantiate (stage02);
+			break;
+		case 3:
+			Instantiate (stage03);
+			break;
 		}
-	}
-
-	// マップブロックで空白以外はWallに変えて当たりをつける
-	public void changeLayerNameToWall (){
-
-		GameObject[] emptyBlocks = GameObject.FindGameObjectsWithTag ("MapBlock");
-		Debug.Log (emptyBlocks.Length);
-		foreach(GameObject block in emptyBlocks) {
-			var scr = block.GetComponent<OrientedBlock>();
-			if (scr != null) {
-				BlockOrientation num = scr.GetOrientation ();
-				if ( num != BlockOrientation.Empty ) {
-					block.layer = LayerMask.NameToLayer ("Wall");
-				}
-			}
-		}
-
-//		GameObject[] emptyBlocks = GameObject.FindGameObjectsWithTag ("EmptyBlock");
-//		foreach(GameObject block in emptyBlocks) {
-//			block.layer = LayerMask.NameToLayer ("Untagged");	//名前から番号[int]
-//		}
-//
-//		GameObject[] emptyCubes = GameObject.FindGameObjectsWithTag ("EmptyCube");
-//
-//		foreach(GameObject block in emptyCubes) {
-//			block.layer = LayerMask.NameToLayer ("Untagged");	//名前から番号[int]
-//		}
-	}
-
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
