c262a04f3507eabf197bf3dfc28c119f52d2bd69
KEI SHIABTA <info@pistolstarweb.com>
2018-02-04 05:40:41 +0900

    通常弾PUの作成　イルミネーションウォールの作成

---
+++ Assets/PURotater.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// PURotaterの説明
+/// </summary>
+public class PURotater : MonoBehaviour {
+
+	#region  メンバ変数
+	public float rotateSpeed=0.1f;
+	Quaternion originalRot;
+	public Vector3 axis;
+	#endregion
+	void Start(){
+		originalRot = transform.rotation;
+	}
+
+	#region  Update
+	float degrees;
+	float degrees2;
+	float degrees3;
+	void Update(){
+		degrees+=rotateSpeed*Time.deltaTime;
+
+		transform.rotation = Quaternion.Slerp( transform.rotation, Quaternion.AngleAxis(degrees,axis), Time.deltaTime );
+	}
+
+	#endregion
+
+
+}
