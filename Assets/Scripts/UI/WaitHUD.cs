34367e0561f05f92c5d95bea9ab5e181a73056ce
柴田圭 <info@pistolstarweb.com>
2018-02-12 20:16:57 +0900

    waitHUDの改良：ストップ時にも動くように

--- Assets/Scripts/UI/WaitHUD.cs
+++ Assets/Scripts/UI/WaitHUD.cs
@@ -46,19 +46,20 @@ namespace PSGUI{
 			NGUITools.SetActive(mTweenRot2.gameObject,true);
 			NGUITools.SetActive(mLb.gameObject,false);
 			mLb.text="";
-
+			time=Time.realtimeSinceStartup;
 			rotC=StartCoroutine(Rotate());
 
 		}
 		Coroutine rotC;
-
+		float time=0.0f;
 		bool isInivokin=false;
 		IEnumerator Rotate(){
 			if(isInivokin)yield break;
 			isInivokin=true;
 			while(true){
-				mTweenRot1.transform.Rotate(Vector3.forward * Time.deltaTime*rotaterDulation);
-				mTweenRot2.transform.Rotate(-Vector3.forward * Time.deltaTime*rotaterDulation*5f);
+				mTweenRot1.transform.Rotate(Vector3.forward * (Time.realtimeSinceStartup-time)*rotaterDulation);
+				mTweenRot2.transform.Rotate(-Vector3.forward *  (Time.realtimeSinceStartup-time)*rotaterDulation*5f);
+				time=Time.realtimeSinceStartup;
 				yield return null;
 			}
 
@@ -77,6 +78,7 @@ namespace PSGUI{
 
 			NGUITools.SetActive(mLb.gameObject,true);
 			mLb.text=labelString;
+			time=Time.realtimeSinceStartup;
 			rotC=StartCoroutine(Rotate());
 			
 		}
