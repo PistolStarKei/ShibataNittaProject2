25b1983feaaaa3376eb300dc367b92934027c265
柴田圭 <info@pistolstarweb.com>
2018-03-09 12:05:01 +0900

    LoadingScene:プログレスバーの追加

--- Assets/Scripts/LoadingSceneManager.cs
+++ Assets/Scripts/LoadingSceneManager.cs
@@ -6,7 +6,9 @@ using UnityEngine.SceneManagement;
 public class LoadingSceneManager : MonoBehaviour {
 
 	#region  メンバ変数
-	
+	public UISlider slider;
+	public int maxProgress;
+	bool isLoaded=false;
 	#endregion
 
 	#region  Public変数
@@ -30,12 +32,20 @@ public class LoadingSceneManager : MonoBehaviour {
 	#region  Public関数
 	public void OnInitProgress(int progress){
 		Debug.Log(""+progress);
-
+		slider.value=GetProgress(progress);
+		if(progress==maxProgress && !isLoaded){
+			isLoaded=true;
+			LoadLobby();
+		}
 	}
 
 	#endregion
 
 	#region  メンバ関数
+	float GetProgress(int progress){
+		float num=progress/maxProgress;
+		return num;
+	}
 	void LoadLobby(){
 		SceneManager.LoadScene("LobbyScene", LoadSceneMode.Single);
 	}
